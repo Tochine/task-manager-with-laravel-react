ee3838e5e0bc7172b965f6cbea648b6c0679cb43
Toshine <toshineluv99@yahoo.com>
2021-01-13 23:10:25 +0100

    project initial setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div id="example"></div>
+            <div class="card">
+                <div class="card-header">{{ __('Home Page') }}</div>
+
+                <div class="card-body">
+                    
+
+                    {{ __('Welcome Home!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
